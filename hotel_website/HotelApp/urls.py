f10445f8742217993a775bfe8d6e129349a0c500
Nils Vos <157572@student.horizoncollege.nl>
2025-02-12 12:01:20 +0100

    navbar gemaakt
    navbar gemaakt en begin gemaakt aan de homepagina

--- hotel_website/HotelApp/urls.py
+++ hotel_website/HotelApp/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('kamers/', views.kamers, name='kamers'),
+    path('restaurants/', views.restaurants, name='restaurants'),
+    path('over_ons/', views.over_ons, name='over_ons'),
+    path('contact/', views.contact, name='contact'),
+
 ]
--- hotel_website/HotelApp/views.py
+++ hotel_website/HotelApp/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render, HttpResponse
 def home(request):
      return render(request, 'home.html')
 
+def kamers(request):
+    return render(request, 'kamers.html')
+
+def restaurants(request):
+    return render(request, 'restaurants.html')
+
+def over_ons(request):
+    return render(request, 'over_ons.html')
+
+def contact(request):
+    return render(request, 'contact.html')
