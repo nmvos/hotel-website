4fed13519d47b4153d4819be83064154f7718072
Nils Vos <157572@student.horizoncollege.nl>
2025-02-14 09:26:16 +0100

    context processor toegevoegd
    context processor toegevoegd, zodat de weather api op elke pagina werkt

---
+++ hotel_website/HotelApp/context_processors.py
@@ -0,0 +1,31 @@
+import requests
+
+def weather_data(request):
+    api_key = '969c22d28f33d607faa1dce4ef079679'
+    url = "https://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "q": "Alkmaar",
+        "appid": api_key,
+        "units": "metric",
+        "lang": "nl"
+    }
+
+    try:
+        r = requests.get(url=url, params=params)
+        res = r.json()
+
+        weather = {
+            "description": res["weather"][0]["description"],
+            "icon": res["weather"][0]["icon"],
+            "temp": res["main"]["temp"],
+            "city": res["name"]
+        }
+    except Exception as e:
+        weather = {
+            "description": "Niet beschikbaar",
+            "icon": "01d",  # Placeholder icoon
+            "temp": "--",
+            "city": "Onbekend"
+        }
+
+    return {"weather": weather}
--- hotel_website/HotelApp/views.py
+++ hotel_website/HotelApp/views.py
@@ -5,27 +5,8 @@ import requests, json
 
 
 def home(request):
-   api_key = '969c22d28f33d607faa1dce4ef079679'
-   url = "https://api.openweathermap.org/data/2.5/weather"
-   params = {
-       "q": "Alkmaar",
-       "appid": api_key,
-       "units": "metric",
-       "lang": "nl"
-   }
-   
-   r = requests.get(url=url, params=params)
-   res = r.json() # converteert de data naar json
 
-# weather dictionary
-   weather = {
-    "description": res["weather"][0]["description"],
-    "icon": res["weather"][0]["icon"],
-    "temp": res["main"]["temp"],
-    "city": res["name"]
-   }
-   
-   return render(request, "home.html", {"weather": weather})
+    return render(request, "home.html")
   
 
 def kamers(request):
