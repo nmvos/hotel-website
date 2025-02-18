127d43af932d045446de12df9fd426ddc4fdffa1
abdigani <dahirabdigani5@gmail.com>
2025-02-18 11:30:20 +0100

    contact.html
    contact pagina + css aangemaakt en het verwerk gedeelte ook

--- hotel_website/HotelApp/views.py
+++ hotel_website/HotelApp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
-import requests, json
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.conf import settings
+from django.http import HttpResponse
 
 # Create your views here.
 
@@ -20,3 +22,22 @@ def over_ons(request):
 
 def contact(request):
     return render(request, 'contact.html')
+
+def verwerken(request):
+    if request.method == 'POST':
+        naam = request.POST.get('naam')
+        email = request.POST.get('email')
+        bericht = request.POST.get('bericht')
+
+        # Process the form data (e.g., send an email)
+        send_mail(
+            f'Contact Form Submission from {naam}',
+            bericht,
+            email,
+            [settings.DEFAULT_FROM_EMAIL],
+            fail_silently=False,
+        )
+
+        return HttpResponse('Bedankt voor je bericht!')
+
+    return redirect('contact')
