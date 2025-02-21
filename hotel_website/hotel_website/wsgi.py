d27c464112b8e920698842dc4b00f3f7ded2bd89
Nils Vos <nilsvos1234@gmail.com>
2025-02-21 10:26:18 +0100

    Update wsgi.py

--- hotel_website/hotel_website/wsgi.py
+++ hotel_website/hotel_website/wsgi.py
@@ -11,8 +11,12 @@ import os
 import sys
 from django.core.wsgi import get_wsgi_application
 
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))  
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_website.settings')
+# Voeg de project root toe aan sys.path
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+# Zet de juiste settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hotel_website.hotel_website.settings')
 
 application = get_wsgi_application()
+
 
